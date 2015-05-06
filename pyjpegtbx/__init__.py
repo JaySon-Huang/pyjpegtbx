d7f95f19f7ae7d09d8d617dfcb0fcaed485ad157
JaySon-Huang <jayson.hjs@gmail.com>
2015-05-06 15:59:59 +0800

    添加保存数据到缓冲区中的功能

--- pyjpegtbx/__init__.py
+++ pyjpegtbx/__init__.py
@@ -107,7 +107,9 @@ class JPEGImage(object):
         if mode == JPEGImage.MODE_DCT:
             obj.data = []
             coef_arrays = jfuncs['jReadCoefs'](ctypes.byref(cinfo))
-            CoefArraysType = ctypes.POINTER(jvirt_barray_ptr * cinfo.num_components)
+            CoefArraysType = ctypes.POINTER(
+                jvirt_barray_ptr * cinfo.num_components
+            )
             coef_arrays = ctypes.cast(coef_arrays, CoefArraysType)
             for i, coef_array in enumerate(coef_arrays.contents):
                 comp = obj.comp_infos[i]
@@ -152,17 +154,7 @@ class JPEGImage(object):
     def copy(self):
         return deepcopy(self)
 
-    def save(self, filename, quality=75):
-        cinfo = jpeg_compress_struct()
-        jerr = jpeg_error_mgr()
-        cinfo.err = jfuncs['jStdError'](ctypes.byref(jerr))
-        jerr.error_exit = error_exit
-        jfuncs['jCreaCompress'](
-            ctypes.byref(cinfo),
-            JPEG_LIB_VERSION, ctypes.sizeof(jpeg_compress_struct)
-        )
-        fp = cfopen(filename.encode(), b'wb')
-        jfuncs['jStdDest'](ctypes.byref(cinfo), fp)
+    def __setcinfo(self, cinfo):
         cinfo.image_width, cinfo.image_height = self.size
         if self.mode == JPEGImage.MODE_RGB:
             cinfo.input_components = 3  # 3 for R,G,B
@@ -214,25 +206,27 @@ class JPEGImage(object):
             coef_arrays = ctypes.cast(
                 cinfo.mem.contents.alloc_small(
                     ctypes.cast(ctypes.byref(cinfo), j_common_ptr),
-                    JPOOL_IMAGE, ctypes.sizeof(jvirt_barray_ptr) * cinfo.num_components
+                    JPOOL_IMAGE,
+                    ctypes.sizeof(jvirt_barray_ptr) * cinfo.num_components
                 ),
                 ctypes.POINTER(jvirt_barray_ptr * cinfo.num_components)
             )
 
             # `min_DCT_h(v)_scaled_size` must be filled
             for i, comp_info in enumerate(comp_infos.contents):
-                comp_info.component_index = self.comp_infos[i]['component_index']
-                comp_info.component_id = self.comp_infos[i]['component_id']
-                comp_info.quant_tbl_no = self.comp_infos[i]['quant_tbl_no']
-                comp_info.ac_tbl_no = self.comp_infos[i]['ac_tbl_no']
-                comp_info.dc_tbl_no = self.comp_infos[i]['dc_tbl_no']
+                ori = self.comp_infos[i]
+                comp_info.component_index = ori['component_index']
+                comp_info.component_id = ori['component_id']
+                comp_info.quant_tbl_no = ori['quant_tbl_no']
+                comp_info.ac_tbl_no = ori['ac_tbl_no']
+                comp_info.dc_tbl_no = ori['dc_tbl_no']
 
-                comp_info.DCT_h_scaled_size = self.comp_infos[i]['DCT_h_scaled_size']
-                comp_info.DCT_v_scaled_size = self.comp_infos[i]['DCT_v_scaled_size']
-                comp_info.h_samp_factor = self.comp_infos[i]['h_samp_factor']
-                comp_info.v_samp_factor = self.comp_infos[i]['v_samp_factor']
-                comp_info.width_in_blocks = self.comp_infos[i]['width_in_blocks']
-                comp_info.height_in_blocks = self.comp_infos[i]['height_in_blocks']
+                comp_info.DCT_h_scaled_size = ori['DCT_h_scaled_size']
+                comp_info.DCT_v_scaled_size = ori['DCT_v_scaled_size']
+                comp_info.h_samp_factor = ori['h_samp_factor']
+                comp_info.v_samp_factor = ori['v_samp_factor']
+                comp_info.width_in_blocks = ori['width_in_blocks']
+                comp_info.height_in_blocks = ori['height_in_blocks']
 
                 # allocate the space of coef arrays
                 coef_array = cinfo.mem.contents.request_virt_barray(
@@ -243,8 +237,8 @@ class JPEGImage(object):
                     comp_info.v_samp_factor
                 )
                 coef_arrays.contents.__setitem__(i, coef_array)
-                min_h = min(min_h, self.comp_infos[i]['DCT_h_scaled_size'])
-                min_v = min(min_v, self.comp_infos[i]['DCT_v_scaled_size'])
+                min_h = min(min_h, ori['DCT_h_scaled_size'])
+                min_v = min(min_v, ori['DCT_v_scaled_size'])
             cinfo.min_DCT_h_scaled_size = min_h
             cinfo.min_DCT_v_scaled_size = min_v
 
@@ -286,10 +280,50 @@ class JPEGImage(object):
                     cinfo.quant_tbl_ptrs[i].contents.quantval[j] = \
                         self.quant_tbls[i]['quantval'][j]
 
+    def save(self, filename, quality=75):
+        cinfo = jpeg_compress_struct()
+        jerr = jpeg_error_mgr()
+        cinfo.err = jfuncs['jStdError'](ctypes.byref(jerr))
+        jerr.error_exit = error_exit
+        jfuncs['jCreaCompress'](
+            ctypes.byref(cinfo),
+            JPEG_LIB_VERSION, ctypes.sizeof(jpeg_compress_struct)
+        )
+        fp = cfopen(filename.encode(), b'wb')
+        jfuncs['jStdDest'](ctypes.byref(cinfo), fp)
+        self.__setcinfo(cinfo)
         # release resources
         jfuncs['jFinCompress'](ctypes.byref(cinfo))
         jfuncs['jDestCompress'](ctypes.byref(cinfo))
         cfclose(fp)
+
+    def save2Bytes(self, quality=75):
+        cinfo = jpeg_compress_struct()
+        jerr = jpeg_error_mgr()
+        cinfo.err = jfuncs['jStdError'](ctypes.byref(jerr))
+        jerr.error_exit = error_exit
+        jfuncs['jCreaCompress'](
+            ctypes.byref(cinfo),
+            JPEG_LIB_VERSION, ctypes.sizeof(jpeg_compress_struct)
+        )
+        outbuffer = ctypes.POINTER(ctypes.c_char)()
+        outsize = ctypes.c_long()
+        jfuncs['jMemDest'](
+            ctypes.byref(cinfo),
+            ctypes.byref(outbuffer),
+            ctypes.byref(outsize)
+        )
+        self.__setcinfo(cinfo)
+        # release resources
+        jfuncs['jFinCompress'](ctypes.byref(cinfo))
+        jfuncs['jDestCompress'](ctypes.byref(cinfo))
+
+        outbuffer = ctypes.cast(
+            outbuffer,
+            ctypes.POINTER(ctypes.c_char * outsize.value)
+        )
+        buf = b''.join(outbuffer.contents)
+        return buf
 
     def color_space_description(self):
         return DESCRIPTIONS_OF_J_COLOR_SAPCE[self._color_space]
